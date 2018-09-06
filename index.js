37aa1c47e2daac4d0d394341e806b83757a039ee
Rebecca Poch <40138374+rebeccapoch@users.noreply.github.com>
2018-09-06 23:35:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi everyone!</h1>
     <p>My name is Rebecca. I'm relatively new to web coding but I like learning and want to improve my skills. This is why I am interested in joining freeCodeCamp's upcoming hackathon, at least online... since I won't be able to get out to San Francisco any time soon. It would be my first hackathon ever!</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    
   </div>
 )
 
