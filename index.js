4a3c959e3e10f8358ac5acce8de13fd0e688cb4f
Rebecca Poch <40138374+rebeccapoch@users.noreply.github.com>
2018-09-06 23:40:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi everyone!</h1>
+    <h1>"Hi everyone!"</h1>
     <p>"My name is Rebecca. I am relatively brand-new to web coding but I like learning and want to improve my skills. This is why I am interested in joining freeCodeCamps upcoming hackathon, at least online... since I will not be able to get out to San Francisco any time soon. It would be my first hackathon ever!"</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
